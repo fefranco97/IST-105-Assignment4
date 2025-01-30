f03400aab3d646030479ad15294a3ecbb8665ad2
Felipe Franco <fe.franco97@gmail.com>
2025-01-29 23:15:33 -0800

    [feat]:minor fixes and css

--- process.php
+++ process.php
@@ -3,7 +3,7 @@
     $b = escapeshellarg($_POST["b"]);
     $c = escapeshellarg($_POST["c"]);
     
-    $command = "pbthon3 calculate.py $a $b $c";
+    $command = "python3 calculate.py $a $b $c";
     $output = shell_exec($command);
     
     echo "<div>$output</div>";
