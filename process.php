659ee0b339b786a174b4168283c5b29383dc70ed
Felipe Franco <fe.franco97@gmail.com>
2025-01-29 22:52:00 -0800

    [fix]:Fixed file name

--- process.php
+++ process.php
@@ -3,7 +3,7 @@
     $b = escapeshellarg($_POST["b"]);
     $c = escapeshellarg($_POST["c"]);
     
-    $command = "pbthon3 process_input.py $a $b $c";
+    $command = "pbthon3 calculate.py $a $b $c";
     $output = shell_exec($command);
     
     echo "<div>$output</div>";
